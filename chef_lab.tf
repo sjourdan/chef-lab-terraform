70ed2e4bfb8c692deed3ae81627b9d02cb26af43
Stephane Jourdan <fasten@fastmail.fm>
2015-04-20 11:45:21 +0200

    added variables, removed useless stuff

--- chef_lab.tf
+++ chef_lab.tf
@@ -3,35 +3,34 @@ provider "digitalocean" {
     token = "${var.do_token}"
 }
 
-# Create a node that will be managed by Chef
 # Configure the SSH Key
 resource "digitalocean_ssh_key" "chef-lab-key" {
     name = "Chef Lab Key"
     public_key = "${file("${var.do_ssh_key_file}.pub")}"
 }
+
+# Create a CentOS 7 node
+# fully updated and installed with latest Chef
 resource "digitalocean_droplet" "chef" {
-  count = 1
-  image = "centos-7-0-x64"
-  name = "chef"
-  region = "lon1"
-  size = "512mb"
-  ssh_keys = [ "728562" ]
-    
+  count = "${var.num_servers}"
+  image = "${var.do_image}"
+  name = "cheflab-${count.index+1}"
+  region = "${var.do_region}"
+  size = "${var.do_size}"
+  ssh_keys = [ "${digitalocean_ssh_key.chef-lab-key.id}" ]
+  user_data = ""
+
   connection {
     user = "root"
-    type = "ssh"
     key_file = "${var.do_ssh_key_file}"
-    timeout = "5m"
-    host = "${digitalocean_droplet.chef.ipv4_address}"
-    port = 22
   }
 
   provisioner "remote-exec" {
-    
+
     inline = [
       "yum update -y",
       "curl -L https://www.chef.io/chef/install.sh | sudo bash"
     ]
-    
+
   }
 }
